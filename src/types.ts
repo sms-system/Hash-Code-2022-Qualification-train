9b2ca6f7ddb177f3c7bb0330b225699219584e1e
Slava Baginov <slava-b@yandex-team.ru>
2023-02-07 20:43:00 +0300

    Add input type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,25 @@
-export type InputData = {};
+export type InputData = {
+    contributors: Array<Contributor>;
+    projects: Array<Project>;
+};
+
+type Contributor = {
+    name: string;
+    skills: Array<Skill>;
+};
+
+type Skill = {
+    name: string;
+    level: number;
+};
+
+type Project = {
+    name: string;
+    daysToComplete: number;
+    score: number;
+    bestBefore: number;
+    roles: Array<Skill>;
+};
 
 export type OutputData = {};
 
