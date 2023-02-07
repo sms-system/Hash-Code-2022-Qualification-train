033e55155a230e6579cb518b287e4f760b8b7989
Slava Baginov <slava-b@yandex-team.ru>
2023-02-07 20:37:34 +0300

    Empty score

--- src/score.ts
+++ src/score.ts
@@ -6,20 +6,8 @@ import assert from 'assert';
 import _ from "lodash";
 
 function parseOutput(outputContent: string): OutputData {
-    const res: OutputData = [];
-    // Array<[number, Set<number>]>
+    const res: OutputData = {};
     const lines = outputContent.split('\n').reverse();
-
-    let n = Number(lines.pop());
-    while (n--) {
-        let [id, ...videoIds] = numbers(lines);
-        res[id] = new Set(videoIds);
-    }
-    for (let i = 0; i < res.length; i += 1) {
-        if (!res[i]) {
-            res[i] = new Set();
-        }
-    }
 
     return res;
 }
@@ -31,31 +19,13 @@ export function calculateScoreFile(inputPath: string, outputPath: string) {
 }
 
 function validateOutput(input: InputData, output: OutputData) {
-    assert.equal(output.length, input.cacheServersCount);
-    for (const videos of output) {
-        const sizeSum = _.sum([...videos].map(id => input.videoSizes[id]));
-        assert(sizeSum <= input.cacheServerSize, `Cache server videos size is ${sizeSum}, expected no more than ${input.cacheServerSize}`);
-    }
+    assert.equal(1, 1);
 }
 
-export function calculateScore(inputPath: string, output: OutputData) {
+export function calculateScore(inputPath: string, output: OutputData): number {
     const input = parseInput(inputPath);
 
     validateOutput(input, output);
 
-    let savedTime = 0;
-    let totalRequestAmount = 0;
-    for (const { videoId, endpointId, requestsAmount } of input.requests) {
-        const { dataCenterlatency, cacheServers } = input.endpoints[endpointId];
-        let bestLatency = dataCenterlatency;
-        for (const { cacheServerId, endpointLatency } of cacheServers) {
-            if (output[cacheServerId].has(videoId)) {
-                bestLatency = Math.min(bestLatency, endpointLatency);
-            }
-        }
-
-        savedTime += requestsAmount * (dataCenterlatency - bestLatency);
-        totalRequestAmount += requestsAmount;
-    }
-    return Math.floor(savedTime * 1000 / totalRequestAmount);
+    return 0;
 }
