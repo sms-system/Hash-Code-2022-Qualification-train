701d0514e9a3513a113c2053dc88284d7fc1ddd2
Alexey Boyko <mralexeyboyko@gmail.com>
2023-02-07 20:46:17 +0300

    add solve draft

--- src/solutions/heuristic.ts
+++ src/solutions/heuristic.ts
@@ -1,6 +1,23 @@
-import { InputData, OutputData } from "../types";
+import { Contributor, Project } from "../types";
+import { InputData } from "../types";
+
+// todo: add heuristics to take mentoring into account
+const assignToProject = (project: Project, contributors: Array<Contributor>) => {
+    // implement this pls
+    return [];
+}
 
 export const solve = ({
-}: InputData): OutputData => {
+    projects,
+    contributors,
+}: InputData): any => {
+    const contributorSkillState = contributors.map(c => [...c.skills]);
+    projects.sort((p1, p2) => (p1.bestBefore - p1.daysToComplete) - (p2.bestBefore - p2.daysToComplete));
+
+    for (const project of projects) {
+        const assignedMembers = assignToProject(project, contributors);
+        // recalculate skills according to mentoring scheme
+    }
+
     return {};
 }
